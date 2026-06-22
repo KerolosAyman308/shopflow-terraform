f1bf8038992bc9c61b1771feb464a50ca157d557
thewitcher7667 <aymanwilliam604@gmail.com>
2026-06-22 21:20:40 +0300

    Remove forgetten outputs

--- azure_terraform/output.tf
+++ azure_terraform/output.tf
@@ -1,17 +1,3 @@
 output "bastion_public_ip" {
   value = module.vm.bastion_public_ip
 }
-
-output "acr_server" {
-  value = module.acr.acr_login_server
-}
-
-
-output "acr_login_user" {
-  value = module.acr.acr_token_username
-}
-
-output "acr_password" {
-  value     = module.acr.acr_token_password
-  sensitive = true
-}
